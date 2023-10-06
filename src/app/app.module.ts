8d3bfbb5985822caf1604a55752c778a8343ca25
Komiljon Najmitdinov <komiljonnajmitdinov99@gmail.com>
2023-10-06 17:30:17 +0500

    add Input Decorator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SvgComponent } from './icon/svg/svg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SvgComponent
   ],
   imports: [
     BrowserModule,
